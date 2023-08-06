bc46f3a9421335bb2eb0eaffe37a507e653e0f62
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-08-06 15:27:56 +0530

    added dashboard and landing page

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      hellooooo
+    </div>
+  )
+}
+
+export default page
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+
+
+const Landingpage = () => {
+  return (
+    <div>
+      Landing page 
+    </div>
+  )
+}
+
+export default Landingpage;
