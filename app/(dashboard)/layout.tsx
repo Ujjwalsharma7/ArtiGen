990a73de668a3866590cfb3f6b680a8dc2f59404
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-08-08 00:59:56 +0530

    added to sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,11 @@
 import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/ui/Sidebar";
 
 const DahboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full relative">
-      <div className="hidden h-full md:flex md:flex-col md:w-72 md:fixed md:inset-y-0 z-[80] bg-purple-950">
-        <div>Sidebaar hhehehhe</div>
+      <div className="hidden h-full md:flex md:flex-col md:w-72 md:fixed bg-slate-600 md:inset-y-0 z-[80]">
+        <Sidebar />
       </div>
       <main className="md:pl-72">
         <Navbar />
---
+++ components/ui/Sidebar.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { Roboto } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import { LayoutDashboard } from "lucide-react";
+
+const roboto = Roboto({
+  weight: "500",
+  subsets: ["latin"],
+});
+
+const routes = [
+  {
+    label: "Dashboard",
+    icon: LayoutDashboard,
+    href: "/dashboard",
+    color: "text-sky-500",
+  },
+];
+
+const Sidebar = () => {
+  return (
+    <div className=" space-y-4 flex flex-col h-full bg-[#3C3F58] text-white">
+      <div className="px-3 py-4 flex-1">
+        <Link href="/dashboard" className="flex items-center pl-3 mb-14">
+          <div className="relative w-14 h-16 flex">
+            <Image fill alt="logo" src="/logo-no-background.png" />
+          </div>
+          <div className=" mx-5">
+            <h1 className={cn("font-bold ", roboto.className)}>
+              Your Imagination
+            </h1>
+          </div>
+        </Link>
+        <div className="space-y-1">
+          {routes.map((route) => (
+            <Link href={route.href} key={route.href}>
+              <div className="flex items-center flex-1">
+                <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+                {route.label}
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
