df44732de51178ae5d48e8640b233c6e6f51db1c
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-08-09 22:49:58 +0530

    made form schema for inputing prompts

--- app/(dashboard)/(routes)/talkative/page.tsx
+++ app/(dashboard)/(routes)/talkative/page.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+"use client";
 
 import Heading from "@/components/Heading";
+import { formSchema } from "./constants";
+import { Form } from "@/components/ui/form";
 
+import * as z from "zod";
 import { MessageCircleIcon } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const Talkative = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
   return (
     <div>
       <Heading
@@ -14,6 +31,13 @@ const Talkative = () => {
         iconColor="text-green-600"
         bgColor="bg-green-600/10"
       />
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}></form>
+          </Form>
+        </div>
+      </div>
     </div>
   );
 };
