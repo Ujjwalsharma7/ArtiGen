2e882d0b8e444925ffa0e3fd09507312842e77a2
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-08-11 23:13:46 +0530

    added bot-avatar

--- components/Empty.tsx
+++ components/Empty.tsx
@@ -6,11 +6,11 @@ interface EmptyProps {
 
 export const Empty = ({ label }: EmptyProps) => {
   return (
-    <div className="h-full p-20 flex flex-col items-center justify-center">
-      <div className="relative h-72 w-72">
+    <div className="h-full p-15 flex flex-col items-center justify-center">
+      <div className="relative h-64 w-64">
         <Image src="/empty1.png" fill alt="Empty" />
       </div>
-      <p className="text-muted-foreground text-sm text-center">{label}</p>
+      <p className="text-muted-foreground text-sm text-center m-4">{label}</p>
     </div>
   );
 };
