7b70d7182d34cad596eb4dae311bb10b776c48b4
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-08-11 23:14:00 +0530

    completed talkative

--- app/(dashboard)/(routes)/talkative/page.tsx
+++ app/(dashboard)/(routes)/talkative/page.tsx
@@ -1,17 +1,31 @@
 "use client";
 
-import Heading from "@/components/Heading";
-import { formSchema } from "./constants";
-import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-
 import * as z from "zod";
-import { MessageCircleIcon } from "lucide-react";
+import axios from "axios";
+import { MessageSquare } from "lucide-react";
 import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { ChatCompletionRequestMessage } from "openai";
 
-const Talkative = () => {
+import { BotAvatar } from "@/components/bot-avatar";
+import Heading from "@/components/Heading";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { cn } from "@/lib/utils";
+import { Loader } from "@/components/Loader";
+import { UserAvatar } from "@/components/user-avatar";
+import { Empty } from "@/components/Empty";
+
+import { formSchema } from "./constants";
+
+const TalkativePage = () => {
+  const router = useRouter();
+
+  const [messages, setMessages] = useState<ChatCompletionRequestMessage[]>([]);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -22,33 +36,62 @@ const Talkative = () => {
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+    try {
+      const userMessage: ChatCompletionRequestMessage = {
+        role: "user",
+        content: values.prompt,
+      };
+      const newMessages = [...messages, userMessage];
+
+      const response = await axios.post("/api/talkative", {
+        messages: newMessages,
+      });
+      setMessages((current) => [...current, userMessage, response.data]);
+
+      form.reset();
+    } catch (error: any) {
+      console.log("error");
+    } finally {
+      router.refresh();
+    }
   };
+
   return (
     <div>
       <Heading
         title="Talk-A-Tive"
-        description="Talk to the smartest AI"
-        icon={MessageCircleIcon}
-        iconColor="text-green-600"
-        bgColor="bg-green-600/10"
+        description="Our most advanced conversation model."
+        icon={MessageSquare}
+        iconColor="text-violet-500"
+        bgColor="bg-violet-500/10"
       />
       <div className="px-4 lg:px-8">
         <div>
           <Form {...form}>
             <form
               onSubmit={form.handleSubmit(onSubmit)}
-              className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+              className="
+                rounded-lg 
+                border 
+                w-full 
+                p-4 
+                px-3 
+                md:px-6 
+                focus-within:shadow-sm
+                grid
+                grid-cols-12
+                gap-2
+              "
             >
               <FormField
-                name="Prompt"
+                name="prompt"
                 render={({ field }) => (
                   <FormItem className="col-span-12 lg:col-span-10">
                     <FormControl className="m-0 p-0">
                       <Input
                         className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
                         disabled={isLoading}
-                        placeholder="How can I help you?"
+                        placeholder="How do I calculate the radius of a circle?"
                         {...field}
                       />
                     </FormControl>
@@ -57,18 +100,44 @@ const Talkative = () => {
               />
               <Button
                 className="col-span-12 lg:col-span-2 w-full"
+                type="submit"
                 disabled={isLoading}
+                size="icon"
               >
-                {" "}
                 Generate
               </Button>
             </form>
           </Form>
         </div>
-        <div className=" space-y-4 mt-4">ANSWER</div>
+        <div className="space-y-4 mt-4">
+          {isLoading && (
+            <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+              <Loader />
+            </div>
+          )}
+          {messages.length === 0 && !isLoading && (
+            <Empty label="No conversation started." />
+          )}
+          <div className="flex flex-col-reverse gap-y-4">
+            {messages.map((message) => (
+              <div
+                key={message.content}
+                className={cn(
+                  "p-8 w-full flex items-start gap-x-8 rounded-lg",
+                  message.role === "user"
+                    ? "bg-white border border-black/10"
+                    : "bg-muted"
+                )}
+              >
+                {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
+                <p className="text-sm">{message.content}</p>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Talkative;
+export default TalkativePage;
