cd1d8674ea903b8f092d971b607632588eda3ff5
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-08-20 17:06:20 +0530

    added layout for landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,20 +1,9 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { LandingNavbar } from "@/components/LandingNavbar";
 
 const Landingpage = () => {
-  return (
-    <div>
-      Landing page
-      <div>
-        <Link href="/sign-in">
-          <Button>Sign-in</Button>
-        </Link>
-        <Link href="/sign-up">
-          <Button>Sign-up</Button>
-        </Link>
-      </div>
-    </div>
-  );
+  return <LandingNavbar />;
 };
 
 export default Landingpage;
