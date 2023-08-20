976cbb2e539ed7043bd36946acf6bf77a5040a6a
Ujjwal Sharma <ujjwalsharma1772003@gmail.com>
2023-08-20 18:07:42 +0530

    completed landing page

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,6 +1,6 @@
 const LandingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="h-full bg-[#7A77B9] overflow-auto">
+    <main className="h-full bg-[#212121] overflow-auto">
       <div className="mx-auto max-w-screen-xl h-full">{children}</div>
     </main>
   );
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,15 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { LandingNavbar } from "@/components/LandingNavbar";
+import { LandingHero } from "@/components/LandingHero";
 
 const Landingpage = () => {
-  return <LandingNavbar />;
+  return (
+    <div className="h-full w-full overflow-hidden">
+      <LandingNavbar />
+      <LandingHero />
+    </div>
+  );
 };
 
 export default Landingpage;
